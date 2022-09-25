77712f2f5e4d748651e41547710c8ed23c0aed25
Andrzej Perczak <linux@andrzejperczak.com>
2022-09-25 13:53:43 +0200

    biometrics: Fix udfps races
    onFingerUp() and onFingerDown() are racey, presumably they can execute
    at the same time causing strange behaviours. One of it is animation
    glitch left after unlock which causes unresponsive touch around fp reader.
    
    Let's synchronize those methods and also remove quirky exclusivity
    mechanism from UdfpsAnimation.
    
    With such approach most of strange udfps-around bugs should be solved.
    
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -1279,7 +1279,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         }
     }
 
-    public boolean isFingerDown() {
+    public synchronized boolean isFingerDown() {
         return mOnFingerDown;
     }
 
@@ -1297,7 +1297,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         updateViewDimAmount();
     }
 
-    private void onFingerDown(
+    private synchronized void onFingerDown(
             long requestId,
             int x,
             int y,
@@ -1353,7 +1353,6 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                 mKeyguardUpdateMonitor.requestFaceAuth(FaceAuthApiRequestReason.UDFPS_POINTER_DOWN);
             }
         }
-        mOnFingerDown = true;
         if (mAlternateTouchProvider != null) {
             mBiometricExecutor.execute(() -> {
                 mAlternateTouchProvider.onPointerDown(requestId, (int) x, (int) y, minor, major);
@@ -1385,12 +1384,13 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         for (Callback cb : mCallbacks) {
             cb.onFingerDown();
         }
-        if (mUdfpsAnimation != null) {
+        if (!mOnFingerDown && mUdfpsAnimation != null) {
             mUdfpsAnimation.show();
         }
+        mOnFingerDown = true;
     }
 
-    private void onFingerUp(long requestId, @NonNull UdfpsView view) {
+    private synchronized void onFingerUp(long requestId, @NonNull UdfpsView view) {
         onFingerUp(
                 requestId,
                 view,
